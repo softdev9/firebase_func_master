081cc995628ae16fd4c09073eac3a598542ce0b2
spiderdev86 <zchingnan86@gmail.com>
2019-06-21 04:09:08 -0500

    add daily job func

--- functions/index.js
+++ functions/index.js
@@ -66,3 +66,128 @@ exports.newuser = functions.database.ref('/users/{uid}/devicetoken').onCreate(ev
         console.log(`new user ${uid}:${nid}`);
         return admin.database().ref('/notification/'+uid+'/'+nid).child(Date.now()).set({type:'welcome',text:'Welcome!'})
 })
+exports.trendee_job = functions.pubsub.topic('trendee-tick').onPublish((event) => {
+    console.log("This job is ran every 10 mins!")
+    admin.database().ref('/challenge').once('value',function(currentday){
+        let curry=currentday.val().y;
+        let currm=currentday.val().m;
+        let currd=currentday.val().d;
+        let y=new Date().getUTCFullYear();
+        let m=new Date().getUTCMonth();
+        let d=new Date().getUTCDate();
+        if(new Date(y,m,d-2).getDate()===currd){
+            admin.database().ref('/challenge').set({y:y,m:m+1,d:d});
+            let returnarray=[];
+            const pointarray = [{point:1000},{point:5000},{point:15000},{point:50000},{point:100000},{point:200000},{point:350000},{point:500000}];
+            let yy=new Date(y,m,d-2).getFullYear();
+            let mm=new Date(y,m,d-2).getMonth()+1;
+            let dd=new Date(y,m,d-2).getDate();
+            if(mm<10)mm="0"+mm;
+            if(dd<10)dd="0"+dd;
+            const challengeday=yy+mm+dd;
+            console.log(challengeday);
+            let num=0;
+            admin.database().ref('/users').once('value',function(snapshot){
+                var usernum = snapshot.numChildren();
+                console.log(`usernum:${usernum}`);
+                snapshot.forEach(function(data){
+                    if(data.val().devicetoken!==""){
+                        const uid=data.key;
+                        const nid=data.val().devicetoken;
+                        console.log(`end challenge ${uid}:${nid}`);
+                        admin.database().ref('/notification/'+uid+'/'+nid).child(Date.now()).set({type:'end',text:'See the results of the challenge!',id:challengeday}).then(
+                            ()=>{
+                                console.log(`new challenge ${uid}:${nid}`);
+                                admin.database().ref('/notification/'+uid+'/'+nid).child(Date.now()).set({type:'start',text:'Welcome! New challenge.'});
+                            }
+                        );
+                        num++;
+                        admin.database().ref('/publication/'+challengeday).once('value',function(dataarray){
+                            var maxvote=0,maximage='',maximagetime=0,piccount=0,maxuser=0;
+                            var picnum=dataarray.numChildren();
+                            dataarray.forEach(function(resultdata){
+                                if(resultdata.val().userid===data.key){
+                                    var totalvote=resultdata.val().totalnumber===undefined?0:resultdata.val().totalnumber;
+                                    var uploadedtime = parseInt(resultdata.key,10);
+                                    var voteuser = resultdata.numChildren();
+                                    if(totalvote>maxvote){
+                                        maxvote=totalvote;
+                                        maximagetime = uploadedtime;
+                                        maxuser = voteuser;
+                                    }else if(totalvote===maxvote && voteuser>maxuser){
+                                        maximagetime = uploadedtime;
+                                        maxuser = voteuser;
+                                    }else if(totalvote===maxvote && voteuser===maxuser && uploadedtime>maximagetime){
+                                        maximagetime=uploadedtime;
+                                        maxuser = voteuser;
+                                    }
+                                }
+                                piccount++;
+                                if(piccount===picnum){
+                                    returnarray.push({
+                                        userid:uid,
+                                        totalvote:maxvote,
+                                        votetime:maximagetime,
+                                        nid:nid,
+                                        voteuser:maxuser
+                                    })
+                                    if(returnarray.length===usernum){
+                                        let array=returnarray.sort((a,b)=>{
+                                            if(a.totalvote!==b.totalvote){
+                                                return b.totalvote-a.totalvote
+                                            }else if(a.voteuser!==b.voteuser){
+                                                return b.voteuser-a.voteuser
+                                            }else{
+                                                return a.votetime-b.votetime
+                                            }
+                                        })
+                                        let count =0;
+                                        if(array.length>0 && array[0].totalvote>0){
+                                            count++;
+                                            console.log(`${array[0].userid} win ${count}`);
+                                            admin.database().ref('/notification/'+array[0].userid+'/'+array[0].nid).child(Date.now()).set({type:'bonus',text:'Bravo! You won a bonus!',id:count});
+                                            admin.database().ref('/users/'+array[0].userid).once('value',function(temp){
+                                                let points = temp.val().points;
+                                                let level = temp.val().level;
+                                                admin.database().ref('/users/'+ array[0].userid).child('points').set(points+350);
+                                                if(points+350>=pointarray[level].point){
+                                                    admin.database().ref('/users/'+ array[0].userid).child('level').set(level+1);
+                                                }
+                                            })
+                                        }
+                                        if(array.length>1 && array[1].totalvote>0){
+                                            count++;
+                                            console.log(`${array[1].userid} win ${count}`)
+                                            admin.database().ref('/notification/'+array[1].userid+'/'+array[1].nid).child(Date.now()).set({type:'bonus',text:'Bravo! You won a bonus!',id:count});
+                                            admin.database().ref('/users/'+array[1].userid).once('value',function(temp){
+                                                let points = temp.val().points;
+                                                let level = temp.val().level;
+                                                admin.database().ref('/users/'+ array[1].userid).child('points').set(points+75);
+                                                if(points+75>=pointarray[level].point){
+                                                    admin.database().ref('/users/'+ array[1].userid).child('level').set(level+1);
+                                                }
+                                            })
+                                        }
+                                        if(array.length>2 && array[2].totalvote>0){
+                                            count++;
+                                            console.log(`${array[2].userid} win ${count}`)
+                                            admin.database().ref('/notification/'+array[2].userid+'/'+array[2].nid).child(Date.now()).set({type:'bonus',text:'Bravo! You won a bonus!',id:count});
+                                            admin.database().ref('/users/'+array[2].userid).once('value',function(temp){
+                                                let points = temp.val().points;
+                                                let level = temp.val().level;
+                                                admin.database().ref('/users/'+ array[2].userid).child('points').set(points+50);
+                                                if(points+50>=pointarray[level].point){
+                                                    admin.database().ref('/users/'+ array[2].userid).child('level').set(level+1);
+                                                }
+                                            })
+                                        }
+                                    }
+                                }
+                            })
+                        })
+                    }
+                })
+            })
+        }
+    }) 
+});
